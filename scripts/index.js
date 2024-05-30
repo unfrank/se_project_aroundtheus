47310508e364866d07d395c8e19f17ac81de8ee9
Jamie Unthank <jamie@unfrank.com>
2024-05-29 20:42:43 -0400

    P4 S3

--- scripts/index.js
+++ scripts/index.js
@@ -31,3 +31,16 @@ const initialCards = [
 ];
 
 console.log(initialCards);
+
+const profileEditButton = document.querySelector("#profile-edit-button");
+const profileEditModal = document.querySelector("#profile-edit-modal");
+
+profileEditButton.addEventListener("click", () => {
+  profileEditModal.classList.add("modal__opened");
+});
+
+const profileEditCloseButton = document.querySelector(".modal__close");
+
+profileEditCloseButton.addEventListener("click", () => {
+  profileEditModal.classList.remove("modal__opened");
+});
