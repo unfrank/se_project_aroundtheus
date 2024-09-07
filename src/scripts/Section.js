e839fce4d656aa3c9bc7d6ad11b6d4fc78c430c6
James Unthank <james@unfrank.com>
2024-09-06 20:58:47 -0400

    Further refactor

--- src/scripts/Section.js
+++ src/scripts/Section.js
@@ -1,19 +1,20 @@
 export default class Section {
   constructor({ items, renderer }, containerSelector) {
-    this._items = items;
-    this._renderer = renderer;
-    this._container = document.querySelector(containerSelector);
+    this._items = items; // Data to render
+    this._renderer = renderer; // Function to render each item
+    this._container = document.querySelector(containerSelector); // Container for cards
   }
 
-  // Renders all items by calling the renderer function on each item
+  // Renders all items on initialization
   renderItems() {
     this._items.forEach((item) => {
-      this._renderer(item);
+      this.addItem(item); // Render each item by calling addItem
     });
   }
 
-  // Adds a DOM element to the container
-  addItem(element) {
-    this._container.append(element);
+  // Add a new card to the container and use the renderer
+  addItem(itemData) {
+    const renderedItem = this._renderer(itemData); // Render the item
+    this._container.prepend(renderedItem); // Add it to the DOM
   }
 }
