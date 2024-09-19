2ab9c46376e3d4de11d931d35af088c083f5a761
James Unthank <james@unfrank.com>
2024-09-19 17:36:16 -0400

    Warm up lap

---
+++ src/components/Api.js
@@ -0,0 +1,19 @@
+class Api {
+  constructor(options) {
+    // constructor body
+  }
+
+  getInitialCards() {
+    // ...
+  }
+
+  // other methods for working with the API
+}
+
+const api = new Api({
+  baseUrl: "https://around-api.en.tripleten-services.com/v1",
+  headers: {
+    authorization: "c56e30dc-2883-4270-a59e-b2f7bae969c6",
+    "Content-Type": "application/json",
+  },
+});
